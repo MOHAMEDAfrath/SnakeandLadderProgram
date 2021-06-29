8677fa851c725f0226bc7bba92afd33d9aec697d
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-29 13:02:30 +0530

    [Afrath] Updated the file with Initializing Player UC

--- SnakeandLadderProgram/Program.cs
+++ SnakeandLadderProgram/Program.cs
@@ -2,11 +2,17 @@
 
 namespace SnakeandLadderProgram
 {
+    /// <summary>
+    /// UC1: Initializing Player and start position.
+    /// </summary>
     class Program
     {
+        public const int START_POSITION = 0;
+        public const int BOARD_SIZE = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int player1Position = START_POSITION;
+            Console.WriteLine("Welcome to Snake Ladder Program!");
         }
     }
 }
