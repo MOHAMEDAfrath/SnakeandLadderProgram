2e0a1677c6a4f32d4ca462faef5c8cfbef0f5269
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-29 13:07:53 +0530

    [Afrath] Updated the file with Die service UC

--- SnakeandLadderProgram/Program.cs
+++ SnakeandLadderProgram/Program.cs
@@ -4,6 +4,7 @@ namespace SnakeandLadderProgram
 {
     /// <summary>
     /// UC1: Initializing Player and start position.
+    /// UC2: Using Random to give dice values.
     /// </summary>
     class Program
     {
@@ -13,6 +14,17 @@ namespace SnakeandLadderProgram
         {
             int player1Position = START_POSITION;
             Console.WriteLine("Welcome to Snake Ladder Program!");
+            Program.PlayerOne();
         }
+        public static void PlayerOne()
+        {
+            Console.WriteLine(Program.RollDice());
+        }
+        public static int RollDice()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
+        }
+
     }
 }
